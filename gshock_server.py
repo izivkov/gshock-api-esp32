fe98bcb01d7ed0c394f0f9bacb8dbc7263c38354
Ivo Zivkov <izivkov@gmail.com>
2025-09-08 00:10:52 -0400

    WIP

--- gshock_server.py
+++ gshock_server.py
@@ -30,7 +30,9 @@ async def main():
     try:
         time_task = PeriodicTaskRunner(set_server_time, interval_sec=86400, run_immediately=True)
         await time_task.start(block_until_first_run=True)
-        display.show_message(f"Time on Server: {utils.format_time(time.localtime())}")                
+
+        display.show_message(f"Time on Server: {utils.format_time(time.localtime()}")                
+
         time.sleep(2)
 
         dim_display = DimDisplay(display, touch)
@@ -55,6 +57,8 @@ async def gshock_server():
         "DW-H5600", "OCW-S400", "OCW-S400SG", "OCW-T200SB",
         "ECB-30", "ECB-20", "ECB-10", "ECB-50", "ECB-60", "ECB-70"
     ]
+
+    config_manager.load()
 
     prompt()
 
@@ -82,7 +86,9 @@ async def gshock_server():
 
             # Update store
             t = time.localtime()  # (year, month, mday, hour, minute, second, weekday, yearday)
-            formatted_time = "{:02d}/{:02d} {:02d}:{:02d}".format(t[1], t[2], t[3], t[4])
+            date_fmt = config_manager.get("dateformat", "MM/DD")
+            time_fmt = config_manager.get("timeformat", "24H")
+            formatted_time = f'{utils.format_month_day(t, order=date_fmt)} {utils.format_time(t, timeformat=time_fmt)}'
 
             store.add("last_connected", formatted_time)
             store.add("watch_name", watch_info.name)
@@ -95,12 +101,7 @@ async def gshock_server():
 
             await api.set_time(offset=fine_adjustment_secs)
 
-            now_tuple = time.localtime()
-            now_str = "{}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
-                now_tuple[0], now_tuple[1], now_tuple[2],
-                now_tuple[3], now_tuple[4], now_tuple[5]
-            )
-            logger.info("Time set at {} on {}".format(now_str, watch_info.name))
+            logger.info(f"Time set at {utils.format_month_day(t, order=date_fmt)} {utils.format_time(t, timeformat=time_fmt)}")
 
             if pressed_button == WatchButton.LOWER_LEFT:
                 await show_display(api)
@@ -129,8 +130,6 @@ async def gshock_server():
 
 def set_server_time():
     try:
-        config_manager.load()
-
         ssid = config_manager.get("ssid")
         password = config_manager.get("password")
         timezone = config_manager.get("timezone", "UTC")
@@ -206,7 +205,9 @@ async def show_display(api: GshockAPI):
         short_name = ' '.join(name.strip().split()[1:])
 
         t = time.localtime()
-        last_sync = "{:02}/{:02} {:02}:{:02}".format(t[1], t[2], t[3], t[4])
+        timeformat = config_manager.get("timeformat", "24H")
+        dateformat = config_manager.get("dateformat", "MM/DD")
+        last_sync = f"{utils.format_month_day(t, dateformat)} {utils.format_time(t, timeformat)}"
 
         auto_sync="On" if await api.get_time_adjustment() else "Off"
         print(f"Auto Sync: {auto_sync}")
--- lib/utils/utils.py
+++ lib/utils/utils.py
@@ -7,3 +7,40 @@ def format_time(t):
 
     return formatted_time
 
+def format_time(t, timeformat="24H"):
+    hour = t[3]  # tm_hour
+    minute = t[4]  # tm_min
+    second = t[5]  # tm_sec
+
+    if timeformat == "24H":  # 24-hour format
+        if second == 0:
+            return "{:02d}:{:02d}".format(hour, minute)
+        else:
+            return "{:02d}:{:02d}:{:02d}".format(hour, minute, second)
+    else:  # 12-hour format
+        postfix = 'AM'
+        h = hour
+        if hour == 0:
+            h = 12
+        elif hour == 12:
+            postfix = 'PM'
+        elif hour > 12:
+            h = hour - 12
+            postfix = 'PM'
+        if second == 0:
+            return "{}:{:02d} {}".format(h, minute, postfix)
+        else:
+            return "{}:{:02d}:{:02d} {}".format(h, minute, second, postfix)
+
+def format_month_day(t, order="MM:DD"):
+    month = t[1]  # tm_mon
+    day = t[2]    # tm_mday
+
+    if order == "MM/DD":
+        return f"{month:02d}:{day:02d}"
+    elif order == "DD/MM":
+        return f"{day:02d}:{month:02d}"
+    else:
+        raise ValueError("order must be 'MM/DD' or 'DD/MM'")
+
+
