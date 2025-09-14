2d37e741021942e1302b1f20d2e4a9d50e10cdf0
Ivo Zivkov <izivkov@gmail.com>
2025-09-13 20:54:49 -0400

    Adding support for Super Mini ESP32

--- di.py
+++ di.py
@@ -1,5 +1,10 @@
-# A simple Dependency Injection (DI) module to manage what modules to be imported depending on certain conditions.
-# For example, if some devices do not have a display, we can import a duymmy display module instead.
+"""
+A simple Dependency Injection (DI) module to manage which modules should be imported depending on certain conditions.
+For example, if some devices do not have a display, we can import a dummy display module instead.
+
+However, we cannot distinguish programmatically between the two supported boards, "Super Mini ESP32" 
+and "ESP32-C6-Touch-LCD-1.47", so we just pass the imports as they are.
+"""
 
 import os
 
